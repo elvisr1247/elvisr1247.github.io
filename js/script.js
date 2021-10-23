b04bcee09f437a3e088be0cd1c0461b3c13dbd66
Elvis R <91556829+elvisr1247@users.noreply.github.com>
2021-10-23 17:07:41 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -55,3 +55,30 @@ function checkFlexGap(){
     if(!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
+
+
+/////////////////////////////////////////////////////////////////
+// Working Form reomves the thank form that takes u to a different website
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+      document.getElementById("sb").style.display="inline";
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+   
+    document.getElementById("sbfailed").style.display="inline";
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
